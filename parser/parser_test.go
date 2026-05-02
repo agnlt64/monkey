7adf74d0b802b3d0d2ab8ec7f725131e6fac2371
Antonin GENELOT <jaimelesfraisesbleues@gmail.com>
2026-05-02 23:36:02 +0200

    add parser errors

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,17 +9,15 @@ import (
 
 func TestLetStatement(t *testing.T) {
 	input := `
-let x = 5;
-let y = 10;
-let foobar = 123123;
+let x 5;
+let = 10;
+let 123123;
 `
 	l := lexer.New(input)
 	p := New(l)
 
 	program := p.ParseProgram()
-	if program == nil {
-		t.Fatalf("ParseProgram() returned nil")
-	}
+	checkParserErrors(t, p)
 
 	if len(program.Statements) != 3 {
 		t.Fatalf("program.Statements doesn't contain 3 statements, got=%d", len(program.Statements))
@@ -64,4 +62,17 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
